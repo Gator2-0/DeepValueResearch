26fe8428c615a281922097570b62b14d2bad4410
Gator2-0 <jejedech.63@gmail.com>
2024-09-07 17:08:43 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+
+
+namespace DeepValue
+{
+    public static class DeepValue
+    {
+        public static void Main()
+        {
+            Console.WriteLine("DeepValue");
+
+        }
+
+    }
+    
+}
